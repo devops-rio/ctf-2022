3d66eef4011a41e11f34c53c3eba8cd679c42df1
Mauricio Gomes <mauricio.gomes@tiendanube.com>
2022-11-22 10:03:00 -0300

    Capture The Flag 2022

---
+++ app2/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Replace Here!!!</h1>
+        <p>
+          #DockerRio
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
